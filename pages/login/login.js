d1a5df7dc7f1034198cbf6ee7a69c47ffc71ffab
Jacob Uhlig <jacobuhlig@gmail.com>
2022-11-23 10:50:45 +0100

    first upload to this branch

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,27 @@
 
+const URL = "https://spurven-backend.azurewebsites.net"
 
 
-export function initLogin() {
+function initLogin() {
+  document.getElementById("login-button").onclick = login
+}
+
+function login() {
+  console.log("Inden login")  
+  const user = {}
+    user.password = document.getElementById("password").value
+    user.password = document.getElementById("password").value
+
+
+    const options = {}
+    options.method = "GET"
+    options.headers = { "Content-type": "application/json" }
+    options.body = JSON.stringify(user)
+ 
+    fetch(URL + user.username + "/" + user.password, options)
+        .then(r => {
+            console.log("No Data returned from the server")
+            alert("User was succesfully found (or not - we don't know")
+        })
 
 }
