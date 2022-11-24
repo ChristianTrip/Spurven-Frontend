d5e547734f5651e8ddb6058a1654389c3c75615c
tLangelo <theislangelo@gmail.com>
2022-11-24 14:54:34 +0100

    login func

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
 //import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
-import "./navigo_spurven.js"  //Will create the global Navigo, with a few changes, object used below
+import "./navigo_spurven.js"; //Will create the global Navigo, with a few changes, object used below
 //import "./navigo.min.js"  //Will create the global Navigo object used below
 
 import {
-  setActiveLink, adjustForMissingHash, renderTemplate, loadHtml
-} from "./utils.js"
+  setActiveLink,
+  adjustForMissingHash,
+  renderTemplate,
+  loadHtml,
+} from "./utils.js";
 
-import { initLogin } from "./pages/login/login.js"
+import { initLogin } from "./pages/login/login.js";
+
+window.location.href = "http://127.0.0.1:5501/index.html#/login";
 
 window.addEventListener("load", async () => {
+  const templateLogin = await loadHtml("./pages/login/login.html");
 
-  const templateLogin = await loadHtml("./pages/login/login.html")
-
-  adjustForMissingHash()
+  adjustForMissingHash();
 
   const router = new Navigo("/", { hash: true });
   //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
-  window.router = router
+  window.router = router;
 
   router
     .hooks({
       before(done, match) {
-        setActiveLink("menu", match.url)
-        done()
-      }
+        setActiveLink("menu", match.url);
+        done();
+      },
     })
     .on({
       //For very simple "templates", you can just insert your HTML directly like below
-      "/": () => document.getElementById("content").innerHTML = `
-        <h2>Home</h2>
-        <p style='margin-top:1em;font-size: 1.5em;color:darkgray;'>
-          Car's 'R' Us - Created, as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
-        </p>
-     `,
+      "/": () =>
+        (document.getElementById("content").innerHTML = `
+  
+     `),
       "/login": (match) => {
-        renderTemplate(templateLogin, "content")
-        initLogin()
-      }
+        renderTemplate(templateLogin, "content");
+        initLogin();
+      },
     })
     .notFound(() => {
-      renderTemplate(templateNotFound, "content")
+      renderTemplate(templateNotFound, "content");
     })
-    .resolve()
+    .resolve();
 });
 
-
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-  alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
-    + ' Column: ' + column + ' StackTrace: ' + errorObj);
-}
+  alert(
+    "Error: " +
+      errorMsg +
+      " Script: " +
+      url +
+      " Line: " +
+      lineNumber +
+      " Column: " +
+      column +
+      " StackTrace: " +
+      errorObj
+  );
+};
