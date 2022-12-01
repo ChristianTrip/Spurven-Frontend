cb151a850f0123501423ea5320259b9960335219
Andreas-Hjort <wanderhjort@gmail.com>
2022-12-01 17:53:29 +0100

    Calendar is linked to div calendar

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,85 +1,71 @@
-import "../../lib/main.js"
-
-
 
 
 export function initCalendar(){
-  exports.render
-  showCalendar()
+  console.log(5)
+  showCalender();
+
 }
 
-
-
-
-function showCalendar(){
-  var calenderE1 = document.getElementById("calendar")
-  var calendar = new Calendar(calenderE1, {
-    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialDate: '2018-01-12',
-    navLinks: true, // can click day/week names to navigate views
-    editable: true,
-    dayMaxEvents: true, // allow "more" link when too many events
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2018-01-01',
+function showCalender(){
+  console.log(1)
+    const calendarEl = document.getElementById('calendar');
+    console.log("1")
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      initialDate: '2022-11-07',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      {
-        title: 'Long Event',
-        start: '2018-01-07',
-        end: '2018-01-10'
-      },
-      {
-        title: 'Repeating Event',
-        start: '2018-01-09T16:00:00'
-      },
-      {
-        title: 'Repeating Event',
-        start: '2018-01-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2018-01-11',
-        end: '2018-01-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T10:30:00',
-        end: '2018-01-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2018-01-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2018-01-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2018-01-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2018-01-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2018-01-28'
-      }
-    ]
-  });
-
-  calendar.render();
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2022-11-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2022-11-07',
+          end: '2022-11-10'
+        },
+        {
+          title: 'Repeating Event',
+          start: '2022-11-09T16:00:00'
+        },
+        {
+          title: 'Repeating Event',
+          start: '2022-11-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2022-11-11',
+          end: '2022-11-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2022-11-12T10:30:00',
+          end: '2022-11-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2022-11-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2022-11-12T14:30:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2022-11-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2022-11-28'
+        }
+      ]
+    });
+    console.log("2")
+    calendar.render();
+    console.log("3")
 }
-
